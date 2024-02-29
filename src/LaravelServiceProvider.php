cd1582c74fa608c53ab1db74b77a5ad6303d36b2
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2024-02-29 13:59:22 +0400

    feat(Auth Guard): create service provider

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -2,9 +2,30 @@
 
 namespace Kostyap\JwtAuth;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelServiceProvider extends ServiceProvider
 {
+    public const CONFIG_NAME = 'jwt.php';
 
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path(self::CONFIG_NAME),
+        ]);
+
+        $this->extendAuthGuard();
+    }
+
+    protected function extendAuthGuard(): void
+    {
+        Auth::extend('jwt', function (Application $app, string $name, array $config) {
+            return new JWTGuard(
+                $app->make(JWT::class),
+                Auth::createUserProvider($config['provider']),
+            );
+        });
+    }
 }
