55c1dcf08d77fc051859db4ac071af33d06d5db4
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2024-04-01 12:25:55 +0400

    feat(TokenSource): send tokens to response depending on their source

--- stubs/default/app/Http/Controllers/AuthApiController.php
+++ stubs/default/app/Http/Controllers/AuthApiController.php
@@ -7,10 +7,26 @@ use App\Http\Requests\LoginApiRequest;
 use Exception;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Kostyap\JwtAuth\Enum\AccessTokenSource;
+use Kostyap\JwtAuth\Enum\RefreshTokenSource;
 use Kostyap\JwtAuth\Jwt\Data\TokenPair;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class AuthApiController extends Controller
 {
+    private AccessTokenSource $accessTokenSource;
+    private RefreshTokenSource $refreshTokenSource;
+    private int $ttl;
+    private int $refreshTtl;
+
+    public function __construct()
+    {
+        $this->accessTokenSource = config('jwt.token_source.access_token');
+        $this->refreshTokenSource = config('jwt.token_source.refresh_token');
+        $this->ttl = config('jwt.ttl');
+        $this->refreshTtl = config('jwt.refresh_ttl');
+    }
+
     public function login(LoginApiRequest $request): Response
     {
         try {
@@ -24,10 +40,29 @@ class AuthApiController extends Controller
             return new Response(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
         }
 
-        return new Response([
-            'access_token' => $tokenPair->accessToken,
-            'refresh_token' => $tokenPair->refreshToken
-        ]);
+        $response = new Response();
+        $responseContent = [];
+
+        match ($this->accessTokenSource) {
+            AccessTokenSource::Bearer => $responseContent['access_token'] = $tokenPair->accessToken,
+            AccessTokenSource::Cookie => $response->withCookie(Cookie::create(
+                'access_token',
+                $tokenPair->accessToken,
+                $this->ttl
+            )),
+        };
+
+        match ($this->refreshTokenSource) {
+            RefreshTokenSource::Body => $responseContent['refresh_token'] = $tokenPair->refreshToken,
+            RefreshTokenSource::Cookie => $response->withCookie(Cookie::create(
+                'refresh_token',
+                $tokenPair->refreshToken,
+                $this->refreshTtl
+            )),
+        };
+
+        $response->setContent(empty($responseContent) ? 'Authenticated' : $responseContent);
+        return $response;
     }
 
     public function me(): Response
