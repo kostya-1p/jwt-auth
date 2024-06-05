ddd2ac0a557421123797d3848413ea81380827d6
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2024-06-05 16:14:47 +0400

    refactor(dto): tokenpair non empty string docs

--- src/Jwt/Data/TokenPair.php
+++ src/Jwt/Data/TokenPair.php
@@ -4,7 +4,10 @@ namespace Kostyap\JwtAuth\Jwt\Data;
 
 class TokenPair
 {
+    /** @var non-empty-string */
     public string $accessToken;
+
+    /** @var non-empty-string */
     public string $refreshToken;
 
     public static function make(string $accessToken, string $refreshToken): TokenPair
